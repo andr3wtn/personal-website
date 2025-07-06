9aaf89a5a9a571a32c076a03874ac819ca65acf9
Andrew Tian <144362414+andr3wtn@users.noreply.github.com>
2025-07-06 14:31:41 +0800

    style: added background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Contact from "./components/Contact"
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gradient-to-r from-[#e3f2fd] to-[#a7ffeb]">
       <Landing />
 
       <About />
