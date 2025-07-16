280f6c444bb2cd6378ccf26eebc426b7cdef2c74
Andrew Tian <144362414+andr3wtn@users.noreply.github.com>
2025-07-16 17:59:39 +0800

    Commit changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Replace 'your-repo-name' with your actual GitHub repo name
+  assetPrefix: "/personal-website/",
+  // If your site is not at the root, set basePath as well:
+  // basePath: "/personal-website",
 };
 
 export default nextConfig;
